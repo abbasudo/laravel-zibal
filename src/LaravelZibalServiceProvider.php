a8fa05afba3b90273e896e0d4562765b792a8150
Abbas mkhzomi <llabbasmkhll@gmail.com>
2021-09-02 00:19:40 +0430

    zibal class completed , config file added and registered

--- src/LaravelZibalServiceProvider.php
+++ src/LaravelZibalServiceProvider.php
@@ -2,16 +2,39 @@
 
 namespace Llabbasmkhll\LaravelZibal;
 
+use Illuminate\Support\ServiceProvider;
 
 class LaravelZibalServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register()
     {
-        //
+        $this->app->bind(
+            'zibal',
+            function ($app) {
+                return new Zibal();
+            }
+        );
+
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'zibal');
     }
 
+    /**
+     * Publish the plugin configuration.
+     */
     public function boot()
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->publishes(
+                [
+                    __DIR__.'/../config/config.php' => config_path('zibal.php'),
+                ],
+                'config'
+            );
+        }
     }
 }
--- src/Zibal.php
+++ src/Zibal.php
@@ -2,7 +2,85 @@
 
 namespace Llabbasmkhll\LaravelZibal;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
+
+use function PHPUnit\Framework\isEmpty;
+
 class Zibal
 {
+    private $response;
 
+    public function init(
+        int $amount,
+        string $callback,
+        array $callback_params = [],
+        string $description = null,
+        string $orderId = null,
+        string $mobile = null,
+        array $allowedCards = [],
+    ) {
+        if ( ! URL::isValidUrl($callback)) {
+            $callback = URL::route('redirect', $callback_params);
+        }
+
+        $data = [
+            'merchant'    => config('zibal.merchant'),
+            'amount'      => $amount,
+            'callbackUrl' => $callback,
+        ];
+
+        if ( ! is_null($description)) {
+            $data['description'] = $description;
+        }
+
+        if ( ! is_null($orderId)) {
+            $data['orderId'] = $orderId;
+        }
+
+        if ( ! is_null($mobile)) {
+            $data['mobile'] = $mobile;
+        }
+
+        if ( ! isEmpty($allowedCards)) {
+            $data['allowedCards'] = $allowedCards;
+        }
+
+        $this->response = Http::asJson()->acceptJson()->post('https://gateway.zibal.ir/v1/request', $data)->json();
+
+        return $this;
+    }
+
+    public function verify(int $trackId)
+    {
+        $data = [
+            'merchant' => config('zibal.merchant'),
+            'trackId'  => $trackId,
+        ];
+
+        $this->response = Http::asJson()->acceptJson()->post('https://gateway.zibal.ir/v1/verify', $data)->json();
+
+        return $this->response;
+    }
+
+    public function validate(int $code = 422)
+    {
+        abort_if($this->response['result'] != 100, $code);
+
+        return $this;
+    }
+
+    public function redirect(int $trackId = null)
+    {
+        if ( ! is_null($trackId)) {
+            $trackId = $this->response['trackId'];
+        }
+
+        return redirect('https://gateway.zibal.ir/start/'.$trackId);
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
+    }
 }
